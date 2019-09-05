1f2edd658c78d475c8a4312de6803b23643fc6f8
Kerin <kerin.grewal@fmr.com>
2019-09-05 11:54:00 -0400

    fixed changes to make A work

--- FidZuluAngular/src/app/app.module.ts
+++ FidZuluAngular/src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DvdlistComponent } from './dvdlist/dvdlist.component';
-import { LaptoplistComponent } from './laptoplist/laptoplist.component';
+// import { DvdlistComponent } from './dvdlist/dvdlist.component';
+// import { LaptoplistComponent } from './laptoplist/laptoplist.component';
+// import { ToylistComponent } from './toylist/toylist.component';
 import { HttpClientModule } from '@angular/common/http';
-import { ToylistComponent } from './toylist/toylist.component';
 import { BookListComponent } from './componenets/book-list/book-list.component';
 import { FoodListComponent } from './componenets/food-list/food-list.component';
 import { BikeListComponent } from './componenets/bike-list/bike-list.component';
@@ -17,9 +17,9 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 @NgModule({
   declarations: [
     AppComponent,
-    DvdlistComponent,
-    LaptoplistComponent,
-    ToylistComponent,
+    // DvdlistComponent,
+    // LaptoplistComponent,
+    // ToylistComponent,
     BookListComponent,
     FoodListComponent,
     BikeListComponent
